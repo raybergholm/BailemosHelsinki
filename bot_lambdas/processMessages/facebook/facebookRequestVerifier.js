54e2c529af2e8de25f8d5e452006203855740466
Ray Bergholm <nalamint@gmail.com>
2017-12-10 21:32:35 +0200

    bugfix wrong filename and file hierarchy

--- bot_lambdas/processMessages/facebook/facebookRequestVerifier.js
+++ bot_lambdas/processMessages/facebook/facebookRequestVerifier.js
@@ -3,17 +3,17 @@ const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 var crypto = require("crypto");
 
 module.exports = {
-    verifySignature: function(payload) {
+    verifySignature: function (payload) {
         var shasum;
-    
+
         var signature = payload.split('=')[1];
-    
+
         if (signature) {
             shasum = crypto.createHash('sha1');
             shasum.update(FACEBOOK_APP_SECRET);
-    
+
             var digest = shasum.digest("hex");
-    
+
             if (signature === digest) { // TODO: always a mismatch right now, investigate why
                 return true;
             } else {
