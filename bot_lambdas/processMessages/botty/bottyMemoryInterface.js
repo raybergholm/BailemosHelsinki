12be35c0febd656009c6f8c758c49f32f360f756
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2017-12-11 12:05:50 +0200

    add init botty modules

---
+++ bot_lambdas/processMessages/botty/bottyMemoryInterface.js
@@ -0,0 +1,16 @@
+
+var moment = require("moment");
+
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "eu-central-1"
+});
+
+var dynamoDB = new AWS.dynamoDB();
+var s3 = new AWS.S3();
+
+module.exports = {
+    getConversation: function(userId, callback){
+
+    }
+};
