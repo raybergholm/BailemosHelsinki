60a2b7f89fcc530d50fd1c718a05174e95418ca6
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2017-12-12 16:11:38 +0200

    refactor root files to use scoped references

--- bot_lambdas/processMessages/dataStagingInterface.js
+++ bot_lambdas/processMessages/dataStagingInterface.js
@@ -3,12 +3,12 @@
 const DATA_STAGING_BUCKET_NAME = process.env.DATA_STAGING_BUCKET_NAME;
 const EVENT_DATA_FILENAME = process.env.EVENT_DATA_FILENAME;
 
-var AWS = require("aws-sdk");
+let AWS = require("aws-sdk");
 AWS.config.update({
     region: "eu-central-1"
 });
 
-var s3 = new AWS.S3();
+let s3 = new AWS.S3();
 
 module.exports = {
     getEventData: (callback) => {
@@ -16,19 +16,18 @@ module.exports = {
             Bucket: DATA_STAGING_BUCKET_NAME,
             Key: EVENT_DATA_FILENAME
         }, (err, s3Object) => {
-            var stagedData;
             if (err) {
                 console.log("S3 interface error: ", err);
             } else {
-                stagedData = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
+                let stagedData = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
 
                 // Convert all date strings to date objects (all date/time calculations require it, and JSON.stringify will convert back to string correctly)
-                for (var i = 0; i < stagedData.events.length; i++) {
+                for (let i = 0; i < stagedData.events.length; i++) {
                     stagedData.events[i].start_time = new Date(stagedData.events[i].start_time);
                     stagedData.events[i].end_time = new Date(stagedData.events[i].start_time);
 
                     if (stagedData.events[i].event_times) {
-                        for (var j = 0; j < stagedData.events[i].event_times.length; j++) {
+                        for (let j = 0; j < stagedData.events[i].event_times.length; j++) {
                             stagedData.events[i].event_times[j].start_time = new Date(stagedData.events[i].event_times[j].start_time);
                             stagedData.events[i].event_times[j].end_time = new Date(stagedData.events[i].event_times[j].end_time);
                         }
--- bot_lambdas/processMessages/main.js
+++ bot_lambdas/processMessages/main.js
@@ -2,15 +2,15 @@
 
 //---------------------------------------------------------------------------//
 // Built-in modules
-var moment = require("moment");
+let moment = require("moment");
 //---------------------------------------------------------------------------//
 
 //---------------------------------------------------------------------------//
 // Custom modules
 
-var facebookRequestVerifier = require("./facebook/facebookRequestVerifier");
+let facebookRequestVerifier = require("./facebook/facebookRequestVerifier");
 
-var botty = require("./botty/botty");
+let botty = require("./botty/botty");
 
 //---------------------------------------------------------------------------//
 
@@ -20,14 +20,8 @@ exports.handler = (event, context, callback) => {
         // return;  TODO: allow it to pass for now, debug it later
     }
 
-    var response = {
-        isBase64Encoded: false,
-        statusCode: 200,
-        body: "OK"
-    };
-
     if (event.httpMethod === "POST") {
-        var data = JSON.parse(event.body);
+        let data = JSON.parse(event.body);
         if (data) {
             // Make sure this is a page subscription
             if (data.object === "page") {
@@ -56,43 +50,33 @@ exports.handler = (event, context, callback) => {
         } else {
             console.log("POST request body was null");
         }
-
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully received the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
-
-        // TODO: Check if it's ok if the response can be generated & returned at the end, this lambda should execute fast enough
-
-        console.log("Responding with a 200 OK");
-
-        response = {
-            isBase64Encoded: false,
-            statusCode: 200,
-            body: "OK"
-        };
     }
+
+    let response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: "OK"
+    };
 
     console.log("returning the following response: ", JSON.stringify(response));
     callback(null, response);
 };
 
 function handleReceivedMessage(receivedMessage) {
-    var senderId = receivedMessage.sender.id;
+    let senderId = receivedMessage.sender.id;
 
-    var recipientId = receivedMessage.recipient.id;
-    var timeOfMessage = receivedMessage.timestamp;
-    var messageData = receivedMessage.message;
+    let recipientId = receivedMessage.recipient.id;
+    let timeOfMessage = receivedMessage.timestamp;
+    let messageData = receivedMessage.message;
 
     console.log("entire message data structure: ", receivedMessage);
 
     console.log("Received message for user %d and page %d at %d with message:", senderId, recipientId, timeOfMessage);
     console.log("Message data: ", messageData);
 
-    // var messageId = messageData.mid;
-    var messageText = messageData.text;
-    var messageAttachments = messageData.attachments;
+    // let messageId = messageData.mid;
+    let messageText = messageData.text;
+    let messageAttachments = messageData.attachments;
 
     botty.setConversationTarget(senderId);
     botty.readMessage(messageText, messageAttachments);
