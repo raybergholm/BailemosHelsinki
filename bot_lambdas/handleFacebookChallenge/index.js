ed8d11ca183eb874bfd73a46a4e6a41f6bd5791a
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2017-12-12 16:06:29 +0200

    refactor fb challenge lambda to use 'let' scoping

--- bot_lambdas/handleFacebookChallenge/index.js
+++ bot_lambdas/handleFacebookChallenge/index.js
@@ -5,13 +5,13 @@ const FACEBOOK_VERIFY_TOKEN = process.env.FACEBOOK_VERIFY_TOKEN;
 exports.handler = (event, context, callback) => {
     console.log(event);
 
-    var response;
+    let response;
 
-    if(event.httpMethod === "GET"){
-        var verifyToken = event.queryStringParameters["hub.verify_token"];
+    if (event.httpMethod === "GET") {
+        let verifyToken = event.queryStringParameters["hub.verify_token"];
 
         if (verifyToken === FACEBOOK_VERIFY_TOKEN) {
-            var challengeToken = parseInt(event.queryStringParameters["hub.challenge"], 10);
+            let challengeToken = parseInt(event.queryStringParameters["hub.challenge"], 10);
 
             console.log("Responding to Facebook challenge token");
 
@@ -23,7 +23,7 @@ exports.handler = (event, context, callback) => {
         } else {
             console.log("Incorrect validation token received");
 
-            var payload = {
+            let payload = {
                 message: "Error, wrong validation token"
             };
 
