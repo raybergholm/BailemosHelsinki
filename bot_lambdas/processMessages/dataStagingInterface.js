8e8d797509bc5d00c54c4b39e607783144ed2576
Ray Bergholm <nalamint@gmail.com>
2017-12-15 13:44:00 +0200

    remove unncessary date casting

--- bot_lambdas/processMessages/dataStagingInterface.js
+++ bot_lambdas/processMessages/dataStagingInterface.js
@@ -21,25 +21,12 @@ module.exports = {
             if (err) {
                 console.log("S3 interface error: ", err);
             } else {
-                let stagedData = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
+                let data = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
 
-                // Convert all date strings to date objects (all date/time calculations require it, and JSON.stringify will convert back to string correctly)
-                for (let i = 0; i < stagedData.length; i++) {
-                    stagedData[i].start_time = new Date(stagedData[i].start_time);
-                    stagedData[i].end_time = new Date(stagedData[i].start_time);
-
-                    if (stagedData[i].event_times) {
-                        for (let j = 0; j < stagedData[i].event_times.length; j++) {
-                            stagedData[i].event_times[j].start_time = new Date(stagedData[i].event_times[j].start_time);
-                            stagedData[i].event_times[j].end_time = new Date(stagedData[i].event_times[j].end_time);
-                        }
-                    }
-                }
-
-                // console.log(stagedData);
+                // console.log(data);
 
                 if (callback) {
-                    callback(stagedData);
+                    callback(data);
                 }
             }
         });
