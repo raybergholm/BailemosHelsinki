792b3730d062a2208abb3a9af06570debb6e05a8
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2017-12-18 14:35:13 +0200

    fix FB signature verification (WIP)

--- bot_lambdas/processMessages/facebook/facebookRequestVerifier.js
+++ bot_lambdas/processMessages/facebook/facebookRequestVerifier.js
@@ -6,12 +6,62 @@ const crypto = require("crypto");
 
 const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
+const HASH_ALGORITHM = "sha1";
+
 module.exports = {
-    verifySignature: function (payload) {
+    verifySignature: function (inputSignature, requestPayload) {
+        if (!inputSignature || !requestPayload) {
+            throw new Error("Invalid input params to verifySignature function");
+        }
+
+        let signature = inputSignature.split('=')[1];
+
+        if (!signature) {
+            throw new Error("Failed to extract signature from input");
+        }
+
+        let hash = crypto.createHash(HASH_ALGORITHM);
+        hash.update(JSON.stringify(requestPayload));
+        let hashResult = hash.digest("hex");
+
+        console.log("hash result: ", hashResult);
+        console.log("signature from FB: ", signature);
+
+        let verify = crypto.createVerify(HASH_ALGORITHM);
+
+        try {
+            verify.update(JSON.stringify(requestPayload));
+        } catch (err) {
+            console.log("it caught fire and died: ", err.message);
+            console.log(requestPayload);
+            return false;
+        }
+
+        let publicKey = FACEBOOK_APP_SECRET;
+        let result;
+
+        try {
+            result = verify.verify(publicKey, signature);
+        } catch (err) {
+            console.log("it caught fire and died: ", err.message);
+            console.log(requestPayload);
+            return false;
+        }
+
+        if (result) {
+            console.log("verify match");
+        } else {
+            console.log("verify not matched");
+        }
+
+        return result;
+    },
+
+    verifySignatureOld: function (payload) {
         let signature = payload.split('=')[1];
 
         if (signature) {
-            let hash = crypto.createHash("sha1");
+            let hash = crypto.createHash(HASH_ALGORITHM);
             hash.update(FACEBOOK_APP_SECRET);
 
             let digest = hash.digest("hex");
