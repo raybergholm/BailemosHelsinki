1d54ee77e2901b0d48b97a9bed311fc3b07ffdb6
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2017-12-20 13:00:49 +0200

    implement promise chaining for AWS calls

--- bot_lambdas/updateStoredEventData/dataStagingInterface.js
+++ bot_lambdas/updateStoredEventData/dataStagingInterface.js
@@ -13,14 +13,14 @@ const dynamoDB = new AWS.DynamoDB();
 const s3 = new AWS.S3();
 
 module.exports = {
-    getOrganiserData: (callback) => {
+    getOrganiserData: () => {
         let ddbRequest = dynamoDB.scan({
             TableName: EVENT_ORGANISER_TABLE_NAME,
             Limit: 50
         });
 
-        ddbRequest.promise().then(
-            (data) => {
+        return ddbRequest.promise().then(
+            (data) => { // post-processing: format data from raw DynamoDB format to local app JS object
                 let item;
                 let nodes = {};
                 for (let prop in data.Items) {
@@ -35,52 +35,25 @@ module.exports = {
 
                 console.log(JSON.stringify(nodes));
 
-                if (callback) {
-                    callback(nodes);
-                }
+                return nodes;
             },
             (err) => {
                 console.log("error reading DynamoDB: ", err);
+
+                return err;
             }
         );
-
-        // dynamoDB.scan({
-        //     TableName: EVENT_ORGANISER_TABLE_NAME,
-        //     Limit: 50
-        // }, function (err, data) {
-        //     let item;
-        //     let nodes = {};
-        //     if (err) {
-        //         console.log("error reading DynamoDB: ", err);
-        //     } else {
-        //         for (let prop in data.Items) {
-        //             item = data.Items[prop];
-
-        //             nodes[item.NodeId.N] = { // TODO: hardcoding all these S and Ns are a bit silly, sort that out later?
-        //                 Id: item.NodeId.N,
-        //                 Name: item.Name.S,
-        //                 Type: item.NodeType.S
-        //             };
-        //         }
-
-        //         console.log(JSON.stringify(nodes));
-
-        //         if (callback) {
-        //             callback(nodes);
-        //         }
-        //     }
-        // });
     },
 
-    getEventData: (callback) => {
+    getEventData: () => {
         let s3GetRequest = s3.getObject({
             Bucket: DATA_STAGING_BUCKET_NAME,
             Key: EVENT_DATA_FILENAME
         });
 
-        s3GetRequest.promise().then(
+        return s3GetRequest.promise().then(
             (data) => {
-                stagedData = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
+                let stagedData = JSON.parse(data.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
 
                 // Convert all date strings to date objects (all date/time calculations require it, and JSON.stringify will convert back to string correctly)
                 for (let i = 0; i < stagedData.events.length; i++) {
@@ -97,45 +70,14 @@ module.exports = {
 
                 console.log(stagedData);
 
-                if (callback) {
-                    callback(stagedData);
-                }
+                return stagedData;
             },
             (err) => {
                 console.log("S3 getObject error: ", err);
+
+                return err;
             }
         );
-
-        // s3.getObject({
-        //     Bucket: DATA_STAGING_BUCKET_NAME,
-        //     Key: EVENT_DATA_FILENAME
-        // }, (err, s3Object) => {
-        //     let stagedData;
-        //     if (err) {
-        //         console.log("S3 getObject error: ", err);
-        //     } else {
-        //         stagedData = JSON.parse(s3Object.Body.toString()); // This is not redundant weirdness, it's casting binary >>> string >>> JSON
-
-        //         // Convert all date strings to date objects (all date/time calculations require it, and JSON.stringify will convert back to string correctly)
-        //         for (let i = 0; i < stagedData.events.length; i++) {
-        //             stagedData.events[i].start_time = new Date(stagedData.events[i].start_time);
-        //             stagedData.events[i].end_time = new Date(stagedData.events[i].start_time);
-
-        //             if (stagedData.events[i].event_times) {
-        //                 for (let j = 0; j < stagedData.events[i].event_times.length; j++) {
-        //                     stagedData.events[i].event_times[j].start_time = new Date(stagedData.events[i].event_times[j].start_time);
-        //                     stagedData.events[i].event_times[j].end_time = new Date(stagedData.events[i].event_times[j].end_time);
-        //                 }
-        //             }
-        //         }
-
-        //         console.log(stagedData);
-
-        //         if (callback) {
-        //             callback(stagedData);
-        //         }
-        //     }
-        // });
     },
 
     updateEventData: (payload, callback) => {
@@ -145,31 +87,6 @@ module.exports = {
             Body: payload
         });
 
-        s3PutRequest.promise().then(
-            (data) => {
-                console.log("S3 putObject response metadata:", data);
-                if (callback) {
-                    callback(data);
-                }
-            },
-            (err) => {
-                console.log("S3 putObject error: ", err);
-            }
-        );
-
-        // s3.putObject({
-        //     Bucket: DATA_STAGING_BUCKET_NAME,
-        //     Key: EVENT_DATA_FILENAME,
-        //     Body: payload
-        // }, function (err, data) {
-        //     if (err) {
-        //         console.log("S3 putObject error: ", err);
-        //     } else {
-        //         console.log("S3 putObject response metadata:", data);
-        //         if (callback) {
-        //             callback(data);
-        //         }
-        //     }
-        // });
+        return s3PutRequest.promise();
     }
 };
