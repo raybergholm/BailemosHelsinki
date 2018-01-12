aa03eff45ab969800e5e971ae4058b152b0b23dc
378433a4b79880be2f18a1deffbe93cf82895a2a <raymond.bergholm@sanomapro.fi>
2018-01-12 10:16:51 +0200

    add optional handling of colon in timezone string

--- bot_lambdas/updateStoredEventData/utils/utils.js
+++ bot_lambdas/updateStoredEventData/utils/utils.js
@@ -2,10 +2,11 @@
 
 module.exports = {
     parseTimezoneOffset: (dateTimeString) => {
-        let timezoneRegex = /[+-]\d{4}/;
+        let timezoneRegex = /[+-]\d{2}(:?)\d{2}/;
         let result = timezoneRegex.exec(dateTimeString);
         if (result) {
             let sign = result[0][0];
+            result[0] = result[0].replace(":", "");
             return {
                 hours: Number(sign + result[0].substr(1, 2)),
                 minutes: Number(sign + result[0].substr(3, 2))
