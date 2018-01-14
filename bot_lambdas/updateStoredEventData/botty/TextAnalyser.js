cf676c971008b678d6ddf891c79023b477034e01
Ray Bergholm <nalamint@gmail.com>
2018-01-14 22:33:12 +0200

    refactor finder logic into TextAnalyser

--- bot_lambdas/updateStoredEventData/botty/TextAnalyser.js
+++ bot_lambdas/updateStoredEventData/botty/TextAnalyser.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     analyse: (text, keywords, weights) => {
         let collectedResults = {};
@@ -32,6 +33,9 @@ module.exports = {
 =======
     analyse: (text, keywords) => {
         /* 
+=======
+    /* 
+>>>>>>> refactor finder logic into TextAnalyser
         let keywords = {
             prop: {
                 regex: /blah/i,
@@ -39,7 +43,37 @@ module.exports = {
             },
             ...
         }
+<<<<<<< HEAD
         */
 >>>>>>> add initial structure for stand-alone text analysis module
+=======
+    */
+    analyse: (text, keywords, weights) => {
+        let collectedResults = {};
+
+        for (let prop in keywords) {
+            let result = keywords[prop].exec(text);
+            if (result) {
+                collectedResults[prop] += result.length * weights[prop];
+            }
+        }
+
+        return collectedResults;
+    },
+
+    find: (texts, keywords) => {
+        let tags = new Set();
+
+        texts.forEach((text) => {
+            for (let prop in keywords) {
+                let result = keywords[prop].test(text);
+                if (result) {
+                    tags.add(prop);
+                }
+            }
+        });
+
+        return Array.from(tags.values());
+>>>>>>> refactor finder logic into TextAnalyser
     }
 };
