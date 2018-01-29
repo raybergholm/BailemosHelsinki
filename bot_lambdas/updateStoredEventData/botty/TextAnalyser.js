1d63bffffade3c37b4d246a10f4d6ac0d586894f
Ray Bergholm <nalamint@gmail.com>
2018-01-29 20:46:44 +0200

    fix merge conflict

--- bot_lambdas/updateStoredEventData/botty/TextAnalyser.js
+++ bot_lambdas/updateStoredEventData/botty/TextAnalyser.js
@@ -1,11 +1,6 @@
 "use strict";
 
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
     analyse: (text, keywords, weights) => {
         let collectedResults = {};
 
@@ -32,55 +27,5 @@ module.exports = {
         });
 
         return Array.from(tags.values());
-=======
-    analyse: (text, keywords) => {
-        /* 
-=======
-    /* 
->>>>>>> refactor finder logic into TextAnalyser
-        let keywords = {
-            prop: {
-                regex: /blah/i,
-                weight: 1
-            },
-            ...
-        }
-<<<<<<< HEAD
-        */
->>>>>>> add initial structure for stand-alone text analysis module
-=======
-    */
-=======
->>>>>>> fix merge conflict
-=======
-
->>>>>>> refactor finder logic into TextAnalyser
-    analyse: (text, keywords, weights) => {
-        let collectedResults = {};
-
-        for (let prop in keywords) {
-            let result = keywords[prop].exec(text);
-            if (result) {
-                collectedResults[prop] += result.length * weights[prop];
-            }
-        }
-
-        return collectedResults;
-    },
-
-    find: (texts, keywords) => {
-        let tags = new Set();
-
-        texts.forEach((text) => {
-            for (let prop in keywords) {
-                let result = keywords[prop].test(text);
-                if (result) {
-                    tags.add(prop);
-                }
-            }
-        });
-
-        return Array.from(tags.values());
->>>>>>> refactor finder logic into TextAnalyser
     }
 };
