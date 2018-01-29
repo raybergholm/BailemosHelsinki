a21020672ef24fa1c9720993d4b5504f7689f785
Ray Bergholm <nalamint@gmail.com>
2018-01-29 22:34:21 +0200

    add initial test templates

---
+++ test/getStagedEvents.test.js
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+const lambdaHandler = require("../getStagedEvents/main");
+
+describe("", () => {
+
+});
---
+++ test/updateStoredEventData.test.js
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+const lambdaHandler = require("../updateStoredEventData/main");
+
+describe("", () => {
+
+});
