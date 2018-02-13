f6e5f7c656257f41d91adc65b507fe6862025a84
Ray Bergholm <raybergholm@gmail.com>
2018-02-14 00:32:18 +0200

    swap lets to consts

--- bot_lambdas/handleFacebookChallenge/main.js
+++ bot_lambdas/handleFacebookChallenge/main.js
@@ -3,7 +3,7 @@
 const FACEBOOK_VERIFY_TOKEN = process.env.FACEBOOK_VERIFY_TOKEN;
 
 exports.handler = (event, context, callback) => {
-    let response = handleChallengeResponse(event);
+    const response = handleChallengeResponse(event);
 
     console.log("Returning the following response: ", JSON.stringify(response));
     callback(null, response);
@@ -13,23 +13,23 @@ function handleChallengeResponse(event) {
     let response;
 
     if (event.httpMethod === "GET") {
-        let verifyToken = event.queryStringParameters["hub.verify_token"];
+        const verifyToken = event.queryStringParameters["hub.verify_token"];
 
         if (verifyToken === FACEBOOK_VERIFY_TOKEN) {
             console.log("Responding to Facebook challenge token");
 
-            let challengeToken = parseInt(event.queryStringParameters["hub.challenge"], 10);
+            const challengeToken = parseInt(event.queryStringParameters["hub.challenge"], 10);
             response = generateHttpResponse(200, parseInt(challengeToken, 10));
         } else {
             console.log("Incorrect validation token received");
 
-            let payload = {
+            const payload = {
                 message: "Error, wrong validation token"
             };
             response = generateHttpResponse(422, payload);
         }
     } else {
-        let payload = {
+        const payload = {
             message: "Internal Server Error"
         };
         response = generateHttpResponse(500, payload);
