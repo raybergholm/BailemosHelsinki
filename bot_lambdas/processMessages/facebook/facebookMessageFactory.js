4d60ffdf0df27f6d7acb726f677bf8e6e36a1578
Ray Bergholm <raybergholm@gmail.com>
2018-02-13 16:34:56 +0200

    refactor FacebookMessageFactory to accept method chaining and partial arguments

--- bot_lambdas/processMessages/facebook/facebookMessageFactory.js
+++ bot_lambdas/processMessages/facebook/facebookMessageFactory.js
@@ -3,78 +3,82 @@
 const FACEBOOK_PAGE_ID = process.env.FACEBOOK_PAGE_ID;
 
 module.exports = {
-    createMessage: (targetId, text, attachment) => {
-        if (!targetId || (!text && !attachment)) {
-            throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
-        }
-
-        let message = createMessageBase(targetId);
-
-        if (text) {
-            message.message.text = text;
-        }
-        if (attachment) {
-            message.message.attachment = attachment;
-        }
-        return message;
-    },
-
-    createSenderActionMessage: (targetId, action) => {
-        if (!targetId || !action) {
-            throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
-        }
-
+    assignId: (targetId) => {
         return {
-            messaging_type: "RESPONSE", // NOTE: Messenger API v2.2 compliance: this field is mandatory from 07.05.2018 onwards
-            recipient: {
-                id: targetId
+            createMessage: (text, attachment) => {
+                if (!targetId || (!text && !attachment)) {
+                    throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
+                }
+        
+                let message = createMessageBase(targetId);
+        
+                if (text) {
+                    message.message.text = text;
+                }
+                if (attachment) {
+                    message.message.attachment = attachment;
+                }
+                return message;
             },
-            sender: {
-                id: FACEBOOK_PAGE_ID
+        
+            createSenderActionMessage: (action) => {
+                if (!targetId || !action) {
+                    throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
+                }
+        
+                return {
+                    messaging_type: "RESPONSE", // NOTE: Messenger API v2.2 compliance: this field is mandatory from 07.05.2018 onwards
+                    recipient: {
+                        id: targetId
+                    },
+                    sender: {
+                        id: FACEBOOK_PAGE_ID
+                    },
+                    sender_action: action
+                };
             },
-            sender_action: action
+        
+            createGenericMessageTemplate: (elements) => {
+                if (!targetId || !elements) {
+                    throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
+                }
+        
+                let messageTemplate;
+        
+                if (elements && elements.length > 0) {
+                    messageTemplate = createTemplateBase();
+                    messageTemplate.payload = {
+                        template_type: "generic",
+                        elements: elements.map((item) => {
+                            return createTemplateElement(item.title, item.subtitle, item.imageUrl, item.actionUrl);
+                        })
+                    };
+                }
+        
+                return module.exports.createMessage(targetId, null, messageTemplate); // A message template is just a message with an attachment and no text, so we can reuse the other function
+            },
+        
+            createQuickReplyMessage: (text, quickReplies) => {
+                if (!targetId || !quickReplies) {
+                    throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
+                }
+        
+                let message = createMessageBase(targetId);
+        
+                if (text) {
+                    message.message.text = text;
+                }
+        
+                if (quickReplies) {
+                    message.message.quick_replies = quickReplies.map((item) => {
+                        return item.type === "location" ? createLocationQuickReply() : createTextQuickReply(item.text, item.payload, item.imageUrl);
+                    });
+                }
+        
+                return message;
+            }
         };
-    },
-
-    createGenericMessageTemplate: (targetId, elements) => {
-        if (!targetId || !elements) {
-            throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
-        }
-
-        let messageTemplate;
-
-        if (elements && elements.length > 0) {
-            messageTemplate = createTemplateBase();
-            messageTemplate.payload = {
-                template_type: "generic",
-                elements: elements.map((item) => {
-                    return createTemplateElement(item.title, item.subtitle, item.imageUrl, item.actionUrl);
-                })
-            };
-        }
-
-        return module.exports.createMessage(targetId, null, messageTemplate); // A message template is just a message with an attachment and no text, so we can reuse the other function
-    },
-
-    createQuickReplyMessage: (targetId, text, quickReplies) => {
-        if (!targetId || !quickReplies) {
-            throw new Error("Invalid function arguments: cannot create a message with no targetId or empty body");
-        }
-
-        let message = createMessageBase(targetId);
-
-        if (text) {
-            message.message.text = text;
-        }
-
-        if (quickReplies) {
-            message.message.quick_replies = quickReplies.map((item) => {
-                return item.type === "location" ? createLocationQuickReply() : createTextQuickReply(item.text, item.payload, item.imageUrl);
-            });
-        }
-
-        return message;
-    },
+    }
 };
 
 // Everything below here is for creating fragments of a valid message. They're not meant to be publically accessible (which is why they are not in the module.exports section), since anything outside this module should only know "call this method to get a fully formed message"
