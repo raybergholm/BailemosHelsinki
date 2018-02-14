aabb52a30970dda69a4c7f6b3eefc6836571e0e9
Ray Bergholm <raybergholm@gmail.com>
2018-02-14 21:30:27 +0200

    refactor API to receive instance args, implementation of unit test (WIP)

--- bot_lambdas/stageEventData/facebook/facebookApiInterface.js
+++ bot_lambdas/stageEventData/facebook/facebookApiInterface.js
@@ -1,40 +1,35 @@
-const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
+"use strict";
 
-const FACEBOOK_API_VERSION = "v2.11";
+const facebookApiInterface = (apiVersion, accessToken) => {
+    return {
+        getHostUrl: () => "graph.facebook.com",
+        getBatchRequestPath: () => `/${apiVersion}/?access_token=${accessToken}`,
 
-const HOST_URL = "graph.facebook.com";
-const BATCH_PATH = `/${FACEBOOK_API_VERSION}/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
-const RELATIVE_EVENTS_PATH = `/${FACEBOOK_API_VERSION}/events/`;
-const RELATIVE_FEED_PATH = `/${FACEBOOK_API_VERSION}/feed/`;
-
-module.exports = {
-    getHostUrl: () => HOST_URL,
-    getBatchRequestPath: () => BATCH_PATH,
-
-    buildBatchEventQueryPayload: (nodeIds) => {
-        return {
-            relative_url: buildQueryUrl(RELATIVE_EVENTS_PATH, {
-                // debug: "all",
-                time_filter: "upcoming",
-                ids: nodeIds,
-                fields: ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
-            }, true),
-            method: "GET"
-        };
-    },
-    buildBatchFeedQueryPayload: (nodeIds) => {
-        return {
-            relative_url: buildQueryUrl(RELATIVE_FEED_PATH, {
-                // debug: "all",
-                ids: nodeIds,
-                fields: ["type", "link", "message", "story"]
-            }, true),
-            method: "GET"
-        };
-    }
+        buildBatchEventQueryPayload: (nodeIds) => {
+            return {
+                relative_url: buildQueryUrl(`/${apiVersion}/events/`, {
+                    // debug: "all",
+                    time_filter: "upcoming",
+                    ids: nodeIds,
+                    fields: ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
+                }, true),
+                method: "GET"
+            };
+        },
+        buildBatchFeedQueryPayload: (nodeIds) => {
+            return {
+                relative_url: buildQueryUrl(`/${apiVersion}/feed/`, {
+                    // debug: "all",
+                    ids: nodeIds,
+                    fields: ["type", "link", "message", "story"]
+                }, true),
+                method: "GET"
+            };
+        }
+    };
 };
 
-function buildQueryUrl (basePath, params, escapePath) {
+function buildQueryUrl(basePath, params, escapePath) {
     let path = basePath;
     if (params) {
         const paramsArr = [];
@@ -48,3 +43,5 @@ function buildQueryUrl (basePath, params, escapePath) {
     }
     return path;
 }
+
+module.exports = facebookApiInterface;
--- bot_lambdas/stageEventData/tests/facebookApiInterface.test.js
+++ bot_lambdas/stageEventData/tests/facebookApiInterface.test.js
@@ -1,4 +1,45 @@
-
+import { describe, it } from "mocha";
+import { expect } from "chai";
 
 import facebookApiInterface from "../facebook/facebookApiInterface";
+
+// mock AWS env vars
+const apiVersion = "v2.11";
+
+const process = {
+    env: {
+        FACEBOOK_ACCESS_TOKEN: "testtoken123"
+    }
+};
+
+const hostUrl = "graph.facebook.com";
+
+const apiInstance = facebookApiInterface(apiVersion, process.env.FACEBOOK_ACCESS_TOKEN);
+
+describe("Basic sanity checks", () => {
+    it("Host URL is valid", () => {
+        expect(apiInstance.getHostUrl()).to.equal(hostUrl);
+    });
+
+    it("Api version and access token are being passed through", () => {
+        expect(apiInstance.getBatchRequestPath()).to.equal(`/${apiVersion}/?access_token=${process.env.FACEBOOK_ACCESS_TOKEN}`);
+    });
+
+    it("Batch Event Query payloads are the correct format", () => {
+        relative_url: buildQueryUrl(`/${apiVersion}/events/`, {
+            // debug: "all",
+            time_filter: "upcoming",
+            ids: nodeIds,
+            fields: ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
+        }, true),
+        method: "GET"
+
+
+        const expectedOutput = {}; 
+
+        const testInput = ["123", "456", "789", "12345"];
+
+        const batchEventQueryPayload = apiInstance.buildBatchEventQueryPayload();
+    });
+});
 
