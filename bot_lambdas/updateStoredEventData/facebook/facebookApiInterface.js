665c90373de6fb0ff1c0c2928f70421cb58f9bd8
Ray Bergholm <raybergholm@gmail.com>
2018-02-14 15:02:39 +0200

    move batch request payload generation to FacebookApiInterface

--- bot_lambdas/updateStoredEventData/facebook/facebookApiInterface.js
+++ bot_lambdas/updateStoredEventData/facebook/facebookApiInterface.js
@@ -4,27 +4,47 @@ const FACEBOOK_API_VERSION = "v2.11";
 
 const HOST_URL = "graph.facebook.com";
 const BATCH_PATH = `/${FACEBOOK_API_VERSION}/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
-const RELATIVE_EVENTS_PATH = `/${FACEBOOK_API_VERSION}/events?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
-const RELATIVE_FEED_PATH = `/${FACEBOOK_API_VERSION}/feed/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
+const RELATIVE_EVENTS_PATH = `/${FACEBOOK_API_VERSION}/events/`;
+const RELATIVE_FEED_PATH = `/${FACEBOOK_API_VERSION}/feed/`;
 
 module.exports = {
     getHostUrl: () => HOST_URL,
     getBatchRequestPath: () => BATCH_PATH,
-    getRelativeEventsPath: () => RELATIVE_EVENTS_PATH,
-    getRelativeFeedPath: () => RELATIVE_FEED_PATH,
 
-    buildQueryUrl: (basePath, params, escapePath) => {
-        let path = basePath;
-        if (params) {
-            const paramsArr = [];
-            for (const prop in params) {
-                paramsArr.push(prop + "=" + (params[prop] instanceof Array ? params[prop].join(',') : params[prop]));
-            }
-            path += '?' + paramsArr.join('&');
-        }
-        if (escapePath) {
-            path = encodeURIComponent(path);
-        }
-        return path;
+    buildBatchEventPayload: (nodeIds) => {
+        return {
+            relative_url: buildQueryUrl(RELATIVE_EVENTS_PATH, {
+                // debug: "all",
+                time_filter: "upcoming",
+                ids: nodeIds,
+                fields: ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
+            }, true),
+            method: "GET"
+        };
+    },
+    buildBatchFeedPayload: (nodeIds) => {
+        return {
+            relative_url: buildQueryUrl(RELATIVE_FEED_PATH, {
+                // debug: "all",
+                ids: nodeIds,
+                fields: ["type", "link", "message", "story"]
+            }, true),
+            method: "GET"
+        };
     }
 };
+
+function buildQueryUrl (basePath, params, escapePath) {
+    let path = basePath;
+    if (params) {
+        const paramsArr = [];
+        for (const prop in params) {
+            paramsArr.push(prop + "=" + (params[prop] instanceof Array ? params[prop].join(',') : params[prop]));
+        }
+        path += '?' + paramsArr.join('&');
+    }
+    if (escapePath) {
+        path = encodeURIComponent(path);
+    }
+    return path;
+}
