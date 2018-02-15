2f7a3e906936e11208f9a1e332648afffcd89ec3
Ray Bergholm <raybergholm@gmail.com>
2018-02-15 10:44:56 +0200

    refactor FacebookApiInterface to instance-based object

--- bot_lambdas/stageEventData/tests/facebookApiInterface.test.js
+++ bot_lambdas/stageEventData/tests/facebookApiInterface.test.js
@@ -25,21 +25,26 @@ describe("Basic sanity checks", () => {
         expect(apiInstance.getBatchRequestPath()).to.equal(`/${apiVersion}/?access_token=${process.env.FACEBOOK_ACCESS_TOKEN}`);
     });
 
-    it("Batch Event Query payloads are the correct format", () => {
-        relative_url: buildQueryUrl(`/${apiVersion}/events/`, {
-            // debug: "all",
-            time_filter: "upcoming",
-            ids: nodeIds,
-            fields: ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
-        }, true),
-        method: "GET"
-
-
-        const expectedOutput = {}; 
-
+    it("Batch event query payloads are the correct format", () => {
         const testInput = ["123", "456", "789", "12345"];
 
-        const batchEventQueryPayload = apiInstance.buildBatchEventQueryPayload();
+        const expectedUnencodedOutput = `/${apiVersion}/events?time_filter=upcoming&ids=${testInput.join(",")}&fields=name,description,place,start_time,end_time,eent_times,owner,cover,attending_count`; 
+        const expectedEncodedOutput = encodeURIComponent(expectedUnencodedOutput);
+        
+        expect(apiInstance.buildBatchEventQueryPayload(testInput, false)).to.equal(expectedUnencodedOutput);
+        expect(apiInstance.buildBatchEventQueryPayload(testInput, true)).to.equal(expectedEncodedOutput);
+        expect(apiInstance.buildBatchEventQueryPayload(testInput)).to.equal(expectedEncodedOutput);
+    });
+
+    it("Batch feed query payloads are the correct format", () => {
+        const testInput = ["1111", "1234567", "42"];
+        
+        const expectedUnencodedOutput = `/${apiVersion}/feed?ids=${testInput.join(",")}&fields=type,link,message,story`;
+        const expectedEncodedOutput = encodeURIComponent(expectedUnencodedOutput);
+        
+        expect(apiInstance.buildBatchEventQueryPayload(testInput, false)).to.equal(expectedUnencodedOutput);
+        expect(apiInstance.buildBatchEventQueryPayload(testInput, true)).to.equal(expectedEncodedOutput);
+        expect(apiInstance.buildBatchEventQueryPayload(testInput)).to.equal(expectedEncodedOutput);
     });
 });
 
