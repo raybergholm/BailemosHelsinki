57f7da8599adaf7b5ee5c75c772977df9aa6a4c9
Ray Bergholm <raybergholm@gmail.com>
2018-02-15 23:30:56 +0200

    flatten direct event query lambda

--- bot_lambdas/stageEventData/facebook/facebookApiInterface.js
+++ bot_lambdas/stageEventData/facebook/facebookApiInterface.js
@@ -29,37 +29,22 @@ const facebookApiInterface = (apiVersion, accessToken) => {
             const payload = [];
 
             payload.push(
-                buildBatchQueryPayload(
-                    `/${apiVersion}/events/`,
-                    ids.pageIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
-                )
+                buildBatchQueryPayload(`/${apiVersion}/events/`, ids.pageIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"])
             );
 
             payload.push(
-                buildBatchQueryPayload(
-                    `/${apiVersion}/feed/`,
-                    ids.groupIds, ["type", "link", "message", "story"]
-                )
+                buildBatchQueryPayload(`/${apiVersion}/feed/`, ids.groupIds, ["type", "link", "message", "story"])
             );
 
             payload.push(
-                buildBatchQueryPayload(
-                    `/${apiVersion}/events/`,
-                    ids.userIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
-                )
+                buildBatchQueryPayload(`/${apiVersion}/events/`, ids.userIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"])
             );
 
             return request.post(HOST_URL, batchRequestUrl, payload);
         },
 
         sendBatchDirectEventsQuery: (eventIds) => {
-            const payload = eventIds.map((eventId) => {
-                buildBatchQueryPayload(
-                    `${eventId}/`,
-                    eventId, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]
-                );
-            });
-
+            const payload = eventIds.map((eventId) => buildBatchQueryPayload(`${eventId}/`, eventId, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"]));
             return request.post(HOST_URL, batchRequestUrl, payload);
         }
     };
