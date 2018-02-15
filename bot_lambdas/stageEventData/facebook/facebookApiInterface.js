73cb7219b1c7639f1387e5559f54bc9ae3c0d612
Ray Bergholm <raybergholm@gmail.com>
2018-02-15 10:53:09 +0200

    fix invalid method reference

--- bot_lambdas/stageEventData/facebook/facebookApiInterface.js
+++ bot_lambdas/stageEventData/facebook/facebookApiInterface.js
@@ -1,15 +1,28 @@
 "use strict";
 
+const buildBatchQueryPayload = (path, nodeIds, fields, encodeUri = true) => {
+    return {
+        relative_url: buildQueryUrl(path, {
+            // debug: "all",
+            ids: nodeIds,
+            fields: fields
+        }, encodeUri),
+        method: "GET"
+    };
+};
+
 const facebookApiInterface = (apiVersion, accessToken) => {
     return {
         getHostUrl: () => "graph.facebook.com",
         getBatchRequestPath: () => `/${apiVersion}/?access_token=${accessToken}`,
 
+        buildBatchQueryPayload: buildBatchQueryPayload,
+
         buildBatchEventQueryPayload: (nodeIds, encodeUri = true) => {
-            return this.buildBatchQueryPayload(`/${apiVersion}/events/`, nodeIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"], encodeUri);
+            return buildBatchQueryPayload(`/${apiVersion}/events/`, nodeIds, ["name", "description", "place", "start_time", "end_time", "event_times", "owner", "cover", "attending_count"], encodeUri);
         },
         buildBatchFeedQueryPayload: (nodeIds, encodeUri = true) => {
-            return this.buildBatchQueryPayload(`/${apiVersion}/feed/`, nodeIds, ["type", "link", "message", "story"], encodeUri);
+            return buildBatchQueryPayload(`/${apiVersion}/feed/`, nodeIds, ["type", "link", "message", "story"], encodeUri);
         },
         buildBatchDirectEventQueryPayload: (eventIds) => {
             return eventIds.map((eventId) => {
@@ -21,17 +34,6 @@ const facebookApiInterface = (apiVersion, accessToken) => {
                     method: "GET"
                 };
             });
-        },
-
-        buildBatchQueryPayload: (path, nodeIds, fields, encodeUri = true) => {
-            return {
-                relative_url: buildQueryUrl(path, {
-                    // debug: "all",
-                    ids: nodeIds,
-                    fields: fields
-                }, encodeUri),
-                method: "GET"
-            };
         }
     };
 };
