04e5968141ffac72a1d0cf108b444844322fda40
Ray Bergholm <raybergholm@gmail.com>
2018-02-17 02:14:25 +0200

    refactor facebookApiInterface to be instance-based

--- bot_lambdas/processMessages/facebook/facebookApiInterface.js
+++ bot_lambdas/processMessages/facebook/facebookApiInterface.js
@@ -4,52 +4,20 @@ const request = require("../utils/httpsUtils");
 
 //---------------------------------------------------------------------------//
 
-const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
-
-const FACEBOOK_API_VERSION = "v2.11";
-
 const HOST_URL = "graph.facebook.com";
-const BATCH_PATH = `/${FACEBOOK_API_VERSION}/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
-const SEND_MESSAGE_PATH = `/${FACEBOOK_API_VERSION}/me/messages/?access_token=${FACEBOOK_PAGE_ACCESS_TOKEN}`;
-
 
 const facebookApiInterface = (apiVersion, accessToken) => {
     if (!/^v\d{1}\.\d{2}$/.test(apiVersion)) {
         throw new Error("Invalid API version input");
     }
 
+    const SendMessagePath = `/${apiVersion}/me/messages/?access_token=${accessToken}`;
+
     return {
-        sendMessage: () => {
-            
+        sendMessage: (payload) => {
+            return request.post(HOST_URL, SendMessagePath, payload);
         }
     };
 };
 
-module.exports = {
-    sendBatchDataQuery: (ids) => {
-
-    },
-
-    sendBatchDirectEventsQuery: () => {
-
-    },
-
-    getHostUrl: () => HOST_URL,
-    getBatchRequestPath: () => BATCH_PATH,
-    getSendMessagePath: () => SEND_MESSAGE_PATH,
-    
-    buildQueryUrl: (basePath, params, escapePath) => {
-        let path = basePath;
-        if (params) {
-            const paramsArr = [];
-            for (const prop in params) {
-                paramsArr.push(prop + "=" + (params[prop] instanceof Array ? params[prop].join(',') : params[prop]));
-            }
-            path += '?' + paramsArr.join('&');
-        }
-        if (escapePath) {
-            path = encodeURIComponent(path);
-        }
-        return path;
-    }
-};
+module.exports = facebookApiInterface;
